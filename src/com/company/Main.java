0ce8a9161e3cfb0a4a6914b87c3c645d16eeb6d5
Ilia Iliev <iliailiev@softavail.com>
2022-02-28 18:45:53 +0200

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,30 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hi there!");
+
+        String myName = "Mityo";
+        int chislo = 6;
+        int chisloVtoro = 24;
+
+        System.out.println(chislo + chisloVtoro);
+        System.out.println(chislo - chisloVtoro);
+        System.out.println(chislo * chisloVtoro);
+        System.out.println(chislo / chisloVtoro);
+        System.out.println(chislo % chisloVtoro);
+
+        double a = 4.3;
+        double b = 2.7;
+
+        System.out.println(a/b);
+        double cenaBenzin = 2.43;
+        float cenaDizel = 2.66f;
+        boolean istina = true;
+
+        System.out.println(myName);
+        System.out.println(chislo);
+        System.out.println(cenaBenzin);
+        System.out.println(cenaDizel);
+        System.out.println(istina);
+
     }
 }
